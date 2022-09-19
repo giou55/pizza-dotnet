5265fb865bf77a36ac05e3c2f0cc41295b2e4b88
giou55 <giourmetakis00@gmail.com>
2022-09-19 23:41:23 +0300

    changes

--- Pizza-dotnet/Pages/Menu.cshtml.cs
+++ Pizza-dotnet/Pages/Menu.cshtml.cs
@@ -8,6 +8,13 @@ namespace Pizza_dotnet.Pages
     public class MenuModel : PageModel
     {
         public List<PizzaModel> Pizzas = new List<PizzaModel>();
+        public List<OrderModel> Orders = new List<OrderModel>();
+
+        [BindProperty]
+        public PizzaModel Pizza { get; set; }
+
+        public int Quantity { get; private set; }
+        public string PizzaName { get; private set; }
 
         private readonly ApplicationDbContext _context;
         public MenuModel(ApplicationDbContext context)
@@ -18,6 +25,18 @@ namespace Pizza_dotnet.Pages
         public void OnGet()
         {
             Pizzas = _context.Pizzas.ToList();
+            Orders = _context.Orders.ToList();
+        }
+
+        public IActionResult OnPostAdd()
+        {
+            OrderModel order = new OrderModel();
+            order.PizzaName = PizzaName;
+            order.Quantity = Quantity;
+
+            _context.Orders.Add(order);
+            _context.SaveChanges();
+            return RedirectToPage("/Menu");
         }
     }
 }
--- Pizza-dotnet/Pages/Orders.cshtml.cs
+++ Pizza-dotnet/Pages/Orders.cshtml.cs
@@ -1,12 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Pizza_dotnet.Data;
+using Pizza_dotnet.Models;
 
 namespace Pizza_dotnet.Pages
 {
     public class OrdersModel : PageModel
     {
+        public List<OrderModel> Orders = new List<OrderModel>();
+
+        private readonly ApplicationDbContext _context;
+        public OrdersModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public void OnGet()
         {
+            Orders = _context.Orders.ToList();
         }
     }
 }
