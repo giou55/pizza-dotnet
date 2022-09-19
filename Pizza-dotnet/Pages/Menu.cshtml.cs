042bb89df3e11169a0a8814e63691c38d107f4f2
giou55 <giourmetakis00@gmail.com>
2022-09-19 20:43:03 +0300

    changes

--- Pizza-dotnet/Pages/Menu.cshtml.cs
+++ Pizza-dotnet/Pages/Menu.cshtml.cs
@@ -1,12 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Pizza_dotnet.Data;
+using Pizza_dotnet.Models;
 
 namespace Pizza_dotnet.Pages
 {
     public class MenuModel : PageModel
     {
+        public List<PizzaModel> Pizzas = new List<PizzaModel>();
+
+        private readonly ApplicationDbContext _context;
+        public MenuModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public void OnGet()
         {
+            Pizzas = _context.Pizzas.ToList();
         }
     }
 }
