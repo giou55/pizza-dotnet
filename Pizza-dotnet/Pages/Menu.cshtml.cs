e4f4e108eae529ff11e1089f8a97b5ce4c2a87d0
giou55 <giourmetakis00@gmail.com>
2022-10-10 17:47:16 +0300

    changes

--- Pizza-dotnet/Pages/Menu.cshtml.cs
+++ Pizza-dotnet/Pages/Menu.cshtml.cs
@@ -8,16 +8,20 @@ namespace Pizza_dotnet.Pages
     public class MenuModel : PageModel
     {
         public List<PizzaModel> Pizzas = new List<PizzaModel>();
-        public List<OrderModel> Orders = new List<OrderModel>();
+        //public List<OrderModel> Orders = new List<OrderModel>();
 
         [BindProperty]
         public PizzaModel Pizza { get; set; }
+
+        [BindProperty]
+        public OrderModel Order { get; set; }
+
         public string PizzaName { get; private set; }
         public string CustomerName { get; private set; }
         public string CustomerAddress { get; private set; }
         public string CustomerPhone { get; private set; }
         public int Quantity { get; private set; }
-        public string TotalPrice { get; private set; }
+        public float TotalPrice { get; private set; }
         public string Timestamp { get; private set; }
 
         private readonly ApplicationDbContext _context;
@@ -29,17 +33,24 @@ namespace Pizza_dotnet.Pages
         public void OnGet()
         {
             Pizzas = _context.Pizzas.ToList();
-            Orders = _context.Orders.ToList();
+            //Orders = _context.Orders.ToList();
         }
 
         public IActionResult OnPostAdd()
         {
             OrderModel order = new OrderModel();
-            order.PizzaName = PizzaName;
-            order.Quantity = Quantity;
+            order.PizzaName = Order.PizzaName;
+            order.CustomerName = Order.CustomerName;
+            order.CustomerAddress = Order.CustomerAddress;
+            order.CustomerPhone = Order.CustomerPhone;    
+            order.Quantity = Order.Quantity;
+            order.TotalPrice = 0.00f;
+            order.Timestamp = DateTime.Now.ToString();
 
             _context.Orders.Add(order);
             _context.SaveChanges();
+
+            //Console.WriteLine(order.Timestamp);
             return RedirectToPage("/Menu");
         }
     }
